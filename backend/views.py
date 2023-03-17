ec4c2db5da6961563cc4f8573509910d99fe07e8
Richard <richard_stephens@hotmail.co.uk>
2023-03-17 13:11:25 +0000

    initial commit

---
+++ backend/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(
+        request,
+        "index.html",
+        context={
+            "title": "Hello World",
+        },
+    )
